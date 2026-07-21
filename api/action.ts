01e751f51f7bb5f599ffc608829c169cfb5851ca
mansarajkumar18 <manasac2305@gmail.com>
2026-07-21 18:01:19 +0530

    Unified single-project architecture: /admin page route, root serverless API, and cleaned structure

--- api/action.ts
+++ api/action.ts
@@ -1,4 +1,4 @@
-import { readDb, writeDb } from './db.js';
+import { readDb, writeDb } from './_db.js';
 
 export default async function handler(req: any, res: any) {
   res.setHeader('Access-Control-Allow-Origin', '*');
